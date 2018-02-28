c2b2ca1e4eb8711f5deb9d3c4762515b71facc23
dcgutierrez93 <dcgutierrez93@yahoo.com>
2018-02-28 10:05:20 -0600

    Adding routes

--- server 2.js
+++ server 2.js
@@ -3,7 +3,6 @@
 // =============================================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 var db = require("./models");
 
 
@@ -12,20 +11,10 @@ var PORT = process.env.PORT || 3000;
 
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// ======
-// Routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "signup.html"));
-});
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
